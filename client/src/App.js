7c54b3d8edec6344dc7e5cc4a2a49c31bbd78074
Rahul <rahul10kr7@gmail.com>
2023-06-25 14:14:02 +0530

    routing done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home/Home";
+import Register from "./pages/Register/Register";
+import Edit from "./pages/Edit/Edit";
+import Profile from "./pages/Profile/Profile";
+import Headers from "./components/Headers/Headers";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    <h1>Rahul Mern's app</h1>
-    </div>
+    <>
+      <Headers />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/edit/:id" element={<Profile />} />
+        <Route path="/userprofile/:id" element={<Edit />} />
+      </Routes>
+    </>
   );
 }
 
--- client/src/components/Headers/Headers.jsx
+++ client/src/components/Headers/Headers.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Headers = () => {
   return (
     <div>
-      
+      Headers
     </div>
   )
 }
