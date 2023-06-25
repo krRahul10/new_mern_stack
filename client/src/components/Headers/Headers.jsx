5d0e5bf9e616d0f77c647012c5c53e02855b6af9
Rahul <rahul10kr7@gmail.com>
2023-06-25 15:48:23 +0530

    form done

--- client/src/components/Headers/Headers.jsx
+++ client/src/components/Headers/Headers.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Headers = () => {
   return (
-    <div>
-      Headers
-    </div>
-  )
-}
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
-export default Headers
+export default Headers;
--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,9 +1,132 @@
-import React from 'react'
-import './register.css'
-const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+import React, { useState } from "react";
+import "./register.css";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Select from "react-select";
 
-export default Register
+const Register = () => {
+  const [inputdata, setInputData] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    mobile: "",
+    gender: "",
+    location: "",
+  });
+  console.log("inputData", inputdata)
+
+  const [status, setStatus] = useState("Active");
+
+  const [image, setImage] = useState("");
+
+  //select Option
+  const options = [
+    { value: "active", label: "active" },
+    { value: "Inactive", label: "Inactive" },
+  ];
+
+  // setInput value
+
+  const setInputValue = (e) => {
+    const { name, value } = e.target;
+    setInputData({ ...inputdata, [name]: value });
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h2 className="text-center mt-1">Register Your Details</h2>
+        <Card>
+          <div className="profile_div text-center">
+            <img src="/man.png" alt="img" />
+          </div>
+          <Form>
+            <Row>
+              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="fname"
+                  placeholder="Enter Your First Name"
+                  onChange={setInputValue}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="lname"
+                  placeholder="Enter Your Last Name"
+                  onChange={setInputValue}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  placeholder="Enter Your Email"
+                  onChange={setInputValue}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                <Form.Label>Mobile Number</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="mobile"
+                  placeholder="Enter Your Mobile Number"
+                  onChange={setInputValue}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                <Form.Label>Select Your Gender</Form.Label>
+                <Form.Check
+                  type={"radio"}
+                  label={`${`Male`}`}
+                  name="gender"
+                  value={"male"}
+                  onChange={setInputValue}
+                />
+                <Form.Check
+                  type={"radio"}
+                  label={`Female`}
+                  name="gender"
+                  value={"female"}
+                  onChange={setInputValue}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                <Form.Label>Select Your Status</Form.Label>
+                <Select options={options} />
+              </Form.Group>
+              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                <Form.Label>Select Your Profile</Form.Label>
+                <Form.Control
+                  type="file"
+                  placeholder="profile"
+                  name="user_profile"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
+                <Form.Label>Enter Your Location</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Your Location"
+                  name="location"
+                  onChange={setInputValue}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Row>
+          </Form>
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default Register;
